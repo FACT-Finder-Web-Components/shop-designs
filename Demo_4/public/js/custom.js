13bec636e0752fe62e042310a1ee86e2c4166a43
Aleksander Laurowski <ext-a.laurowski@omikron.net>
2019-06-11 14:33:13 +0200

    recommended products init

--- Demo_4/public/js/custom.js
+++ Demo_4/public/js/custom.js
@@ -1,9 +1,6 @@
 
 (function($, viewport) {
-
     $(document).ready(function(){
-        // const navComponent = $('ff-navigation');
-        const navbar = $('#categoriesNavigation');
         const mobileMenuInput =  $('#navigation-menu');
         const mobileMenuLabel =  $('.ffw-navigation-label');
 
@@ -12,33 +9,17 @@
             }
         );
 
-        if (viewport.is('>md')) {
-            // navComponent.data('layout','horizontal');
-            // navbar.removeClass('collapse');
-            // navComponent.attr('mobile','false');
-            // navComponent.attr('flyout','true');
-        } else {
-            // navComponent.data('layout','vertical');
+        if (viewport.is('<=md')) {
             mobileMenuInput.click();
             mobileMenuLabel.hide();
-            // navComponent.attr('mobile','true');
-            // navComponent.attr('flyout','false');
         }
-
 
         $(window).resize(
             viewport.changed(function() {
                 console.log(viewport.current());
-                if(viewport.is('>md')) {
-                    // navComponent.attr('layout','horizontal');
-                    // navbar.removeClass('collapse');
-                    // navComponent.attr('mobile','false');
-                } else {
-                    // navComponent.attr('layout','vertical');
-                    // navComponent.attr('mobile','true');
+                if(viewport.is('<=md')) {
                     mobileMenuInput.click();
                     mobileMenuLabel.hide();
-                   // navbar.removeClass('collapse');
                 }
             })
         );
