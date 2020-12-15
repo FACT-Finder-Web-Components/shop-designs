ef9d7cb0dadc61869705b8df3c5a80114ccbcbe1
Krzysztof Kozłowski <ext-k.kozlowski@omikron.net>
2020-12-15 20:06:55 +0100

    remove spinner

--- Demo_3/src/app.js
+++ Demo_3/src/app.js
@@ -14,17 +14,6 @@ jQuery(function ($) {
     $('#product-color, #product-size').togglebutton();
 });
 
-window.displaySpinner = (result) => {
-    let spinner = document.querySelector('#spinnerWrapper'),
-        page = result.paging ? result.paging : result;
-
-    if (page.currentPage === page.pageCount) {
-        spinner.style.display = "none";
-    } else {
-        spinner.style.display = "block";
-    }
-};
-
 const scrollToTop = () => window.scrollTo(0, 0);
 
 document.querySelector("ff-suggest").addEventListener("suggest-item-clicked", function (event) {
